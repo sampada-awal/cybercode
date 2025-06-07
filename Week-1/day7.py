7a34356221c6c186bd6461061b1a1181938ad9f9
Sampada Awal <139024305+sampada-awal@users.noreply.github.com>
2025-06-07 21:52:22 +0545

    day7 file

---
+++ Week-1/day7.py
@@ -0,0 +1,20 @@
+def is_valid_ip(ip):
+    parts = ip.split(".")
+    if len(parts) != 4:
+        return False
+    for part in parts:
+        if not part.isdigit():
+            return False
+        num = int(part)
+        if num < 0 or num > 255:
+            return False
+        if part != str(num):  
+            return False
+    return True
+
+ip = input("Enter an IP address to validate: ")
+if is_valid_ip(ip):
+    print("Valid IPv4 address.")
+else:
+
+    print("Invalid IPv4 address.")
